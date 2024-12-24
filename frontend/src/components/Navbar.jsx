9d8da695209047d9b56fdd95ea8d8f5184ab8ab6
SonyVansha <sonyvansha@gmail.com>
2024-12-24 09:38:40 +0700

    fix: modify dont use

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { LogOut, reset } from "../features/authSlice";
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { LogOut, reset } from "../features/authSlice";
 
 const Navbar = ({ children }) => {
   // const dispatch = useDispatch();
